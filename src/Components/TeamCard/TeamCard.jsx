8f7692e73ec783751ada90fd7b4b2d06bb436bba
shivam-bhadani <86145793+shivam-bhadani@users.noreply.github.com>
2021-09-22 20:16:04 +0530

    change design3

--- src/Components/TeamCard/TeamCard.jsx
+++ src/Components/TeamCard/TeamCard.jsx
@@ -6,9 +6,9 @@ export const TeamCard = () => {
         <div className="team-card-wrapper">
             <h1 className="team-h">Team</h1>
             <div className="team-card-div-wrapper">
-                <div className="team-card-div">
+                <div className="team-card-div team1">
                     <div className="team-img-div">
-                        <img src="images/team1.jpg" alt="team" />
+                        <img src="images/team1.png" alt="team" />
                     </div>
                     <div className="team-text-div">
                         <h2>PixelNova</h2>
@@ -17,16 +17,16 @@ export const TeamCard = () => {
                 </div>
                 <div className="team-card-div team-card-div2">
                     <div className="team-img-div team-img-div2">
-                        <img src="images/team2.jpg" alt="team" />
+                        <img src="images/team2.png" alt="team" />
                     </div>
                     <div className="team-text-div">
                         <h2>PixelNaut</h2>
                         <div className="twitter-logo-div"><a href=""><img src="images/twitterlogo.png" alt="twitter" className="twitter-logo" /></a></div>
                     </div>
                 </div>
-                <div className="team-card-div">
+                <div className="team-card-div team1">
                     <div className="team-img-div">
-                        <img src="images/team3.jpg" alt="team" />
+                        <img src="images/team3.png" alt="team" />
                     </div>
                     <div className="team-text-div">
                         <h2>PixelNeutron</h2>
