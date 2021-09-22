3ef9788425dc498ad788ef52e4c9774fa5efeca9
shivam-bhadani <86145793+shivam-bhadani@users.noreply.github.com>
2021-09-22 19:26:11 +0530

    design change1

--- src/Pages/RoadmapPage/RoadmapPage.jsx
+++ src/Pages/RoadmapPage/RoadmapPage.jsx
@@ -4,8 +4,10 @@ export const RoadmapPage = () => {
     return (
         <div>
             <div className="roadmap-img-div">
-                <h1 className="roadmap-h">Road Map</h1>
-                <img src="images/roadmap.png" alt="" />
+                <div className="roadmap-h-div">
+                    <h1 className="roadmap-h">Road Map</h1>
+                </div>
+                <img src="images/roadmap.jpg" alt="" />
             </div>
         </div>
     )
