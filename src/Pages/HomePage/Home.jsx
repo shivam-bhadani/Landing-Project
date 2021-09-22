01eb398f0eccdd41623592011df269cdaa5736d2
shivam-bhadani <86145793+shivam-bhadani@users.noreply.github.com>
2021-09-22 18:15:57 +0530

    change home

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
                 <div className="content-container">
                     <div className="title-img"><img src="images/title.png" alt="" /></div>
                     <div className="content">
-                        <p>PixelVerse is a mystical unexplored cosmic universe consisting of 7,777 pixelated extraterrestrial bodies including planets, asteroids, stars and black holes; each with distinct characteristics and interesting features:dizzy::stars:</p>
+                        <p>PixelVerse is a mystical unexplored cosmic universe consisting of 7,777 pixelated extraterrestrial bodies including planets, asteroids, stars and black holes; each with distinct characteristics and interesting.</p>
                         <p>Each NFT is priced at ETH 0.07777. Stay on the lookout for owning your own unique PixelVerse and impress your peers. Your unique NFT is also your ticket to The Elysian, a members exclusive social platform.</p>
                     </div>
                 </div>
