0a1a8ff72ec3f38a3ef4a4627d58bdedbfcb652e
shivam-bhadani <86145793+shivam-bhadani@users.noreply.github.com>
2021-09-24 18:57:59 +0530

    change design12

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -12,7 +12,7 @@ export const Timer = () => {
     let interval = useRef();
 
     const startTimer = () => {
-        let dest = new Date("sep 24, 2021 5:00:00");
+        let dest = new Date("sep 28, 2021 5:00:00");
         interval = setInterval(() => {
             let now = new Date().getTime();
             let diff = dest - now;
@@ -45,10 +45,10 @@ export const Timer = () => {
             <div className="presale"><img src="images/pre sale.png" alt="" /></div>
             <div className="countdown-div">
                 <ul class="timer inline-list">
-                    <li><div id="clock-hrs-left" className="clock-time">{timerDay}</div><div>Days</div></li>
-                    <li><div id="clock-hrs-left" className="clock-time">{timerHr}</div><div>Hrs</div></li>
-                    <li><div id="clock-min-left" className="clock-time">{timerMin}</div><div>Min</div></li>
-                    <li><div id="clock-sec-left" className="clock-time">{timerSec}</div><div>Sec</div></li>
+                    <li><div id="clock-hrs-left" className="clock-time">{timerDay} <span className="part">:</span></div><div>DAYS</div></li>
+                    <li><div id="clock-hrs-left" className="clock-time">{timerHr} <span className="part">:</span></div><div>HRS</div></li>
+                    <li><div id="clock-min-left" className="clock-time">{timerMin} <span className="part">:</span></div><div>MIN</div></li>
+                    <li><div id="clock-sec-left" className="clock-time">{timerSec}</div><div>SEC</div></li>
                 </ul>
             </div>
         </div>
--- src/Pages/RoadmapPage/RoadmapPage.jsx
+++ src/Pages/RoadmapPage/RoadmapPage.jsx
@@ -4,9 +4,6 @@ export const RoadmapPage = () => {
     return (
         <div>
             <div className="roadmap-img-div">
-                <div className="roadmap-h-div">
-                    <div className="roadmap"><img src="images/ROAD MAP.png" alt="" /></div>
-                </div>
                 <img src="images/roadmap.png" alt="" />
             </div>
         </div>
