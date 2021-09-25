040563134643c4fc66891e4cd169f8878179e5a1
shivam-bhadani <86145793+shivam-bhadani@users.noreply.github.com>
2021-09-25 17:21:18 +0530

    added planets

--- src/Pages/PlanetPage/PlanetPage.jsx
+++ src/Pages/PlanetPage/PlanetPage.jsx
@@ -2,26 +2,16 @@ import "./PlanetPage.css";
 
 export const PlanetPage = () => {
     return (
-        <div className="planetPagediv">
-            <div className="planet-div">
-                <div className="planet-img-div">
-                    <video autoPlay loop muted>
-                        <source src="images/planet1.mp4" type="video/mp4" />
-                    </video>
+        <div className="planetPageWrapper">
+            <div className="planetPagediv">
+                <div className="planet-div">
+                    <div className="planet-img-div"><img src="images/planet1.gif" alt="planet" className="planet-img" /></div>
+                    <div className="planet-img-div"><img src="images/planet2.gif" alt="planet" className="planet-img" /></div>
+                    <div className="planet-img-div"><img src="images/planet3.gif" alt="planet" className="planet-img" /></div>
+                    <div className="planet-img-div"><img src="images/planet4.gif" alt="planet" className="planet-img" /></div>
+                    <div className="planet-img-div"><img src="images/planet5.gif" alt="planet" className="planet-img" /></div>
+                    <div className="planet-img-div"><img src="images/planet6.gif" alt="planet" className="planet-img" /></div>
                 </div>
-                <div className="planet-img-div">
-                    <video autoPlay loop muted>
-                        <source src="images/planet2.mp4" type="video/mp4" />
-                    </video>
-                </div>
-                <div className="planet-img-div">
-                    <video autoPlay loop muted>
-                        <source src="images/planet3.mp4" type="video/mp4" />
-                    </video>
-                </div>
-                <div className="planet-img-div"><img src="images/planet4.gif" alt="planet" className="planet-img" /></div>
-                <div className="planet-img-div"><img src="images/planet5.gif" alt="planet" className="planet-img" /></div>
-                <div className="planet-img-div"><img src="images/planet6.gif" alt="planet" className="planet-img" /></div>
             </div>
         </div>
     )
